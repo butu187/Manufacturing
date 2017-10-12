f7f90cb3b8beb5c15ff72282f505dd6c25766d1f
Mrutyunjay swain <mrutyunjay.aei@gmail.com>
2017-10-12 11:36:31 -0500

    base test added

---
+++ sonicVision/src/test/java/com/automation/sonicVision/test/BaseTest.java
@@ -0,0 +1,37 @@
+package com.automation.sonicVision.test;
+
+import java.lang.reflect.Method;
+
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class BaseTest {
+
+	public ExtentReports r;
+	public ExtentTest test;
+	@BeforeMethod
+	public void setUp(Method m){
+		r=new ExtentReports("./reports/report.html",false);
+		test=r.startTest(m.getName());
+
+	}
+	@AfterMethod
+	public void tearDown(ITestResult itest){
+		if(itest.getStatus()==ITestResult.FAILURE)
+		{
+			test.log(LogStatus.FAIL,"Script failed check log for details");
+		}
+		else
+		{
+			
+			test.log(LogStatus.PASS, itest.getName(),itest.getInstanceName());
+		}
+		r.endTest(test);
+		r.flush();
+	}
+}
